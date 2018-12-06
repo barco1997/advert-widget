bcfbecbd7326761962e7864a46b8949854d3d1a5
barco <b3181401@gmail.com>
2018-12-06 05:51:25 +0100

    add: fonts and new functionality of button;

--- src/views/button/button.js
+++ src/views/button/button.js
@@ -4,19 +4,24 @@ import { popup } from "../message/message";
 
 let elements = [];
 let body;
+let button;
 
 export function show(text) {
   // convert plain HTML string into DOM elements
   let temporary = document.createElement("div");
   temporary.innerHTML = html;
-
+  button = temporary.getElementsByClassName("js-button-dialog")[0];
   temporary.getElementsByClassName(
     "js-button-dialog"
   )[0].style.background = text;
   temporary.getElementsByClassName("js-button-dialog")[0].style.color =
     text === "black" ? "white" : "black";
-  temporary.getElementsByClassName("js-button-dialog")[0].onclick = function() {
-    popup("wow");
+
+  temporary.getElementsByClassName("js-button-dialog")[0].onclick = function(
+    event
+  ) {
+    event.preventDefault();
+    initialTouch();
   };
   // append elements to body
   body = document.getElementsByTagName("body")[0];
@@ -33,4 +38,14 @@ export function close() {
     elements.pop().remove();
   }
   body.removeEventListener("click", close);
+}
+export function initialTouch() {
+  body.getElementsByClassName("js-button-text")[0].style.opacity = 1;
+  body.getElementsByClassName("js-button-text")[0].style.height = "42";
+  button.style.width = "278px";
+  button.onclick = function() {
+    popup("wow");
+  };
+
+  body.removeEventListener("click", initialTouch);
 }
