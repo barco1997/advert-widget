4c951911de26bbae1253560f0c642431e2be8a04
valdeadmario <vladimir.vstup@gmail.com>
2020-03-06 23:52:56 +0200

    Add new styles and image

--- src/views/react/components/ConnectionsError/index.js
+++ src/views/react/components/ConnectionsError/index.js
@@ -1,19 +1,75 @@
 import React from "react";
 import styled from "styled-components";
 
-const ErrorWrap = styled.div``;
+import Image from "./error.svg";
 
-const ButtonsWrap = styled.div``;
+const ErrorWrap = styled.div`
+  &&& {
+    display: flex !important;
+    flex-direction: column !important;
+    align-items: center !important;
 
-const ErrorHeader = styled.h3``;
+    font-family: "Montserrat" !important;
+    font-style: normal !important;
+    color: #ffffff !important;
+  }
+`;
 
-const ErrorImg = styled.img``;
+const ButtonsWrap = styled.div`
+  &&& {
+    display: flex !important;
+    justify-content: space-between !important;
 
-const ErrorDescription = styled.p``;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 15px;
+  }
+`;
 
-const RefreshButton = styled.button``;
+const ErrorHeader = styled.h3`
+  &&& {
+    font-weight: 800 !important;
+    font-size: 24px !important;
+    line-height: 140% !important;
+  }
+`;
 
-const EndButton = styled.button``;
+const ErrorDescription = styled.p`
+  &&& {
+    font-weight: 600 !important;
+    font-size: 14px !important;
+    line-height: 170% !important;
+  }
+`;
+
+const RefreshButton = styled.button`
+  &&& {
+    border-radius: 5px !important;
+    border: none !important;
+    &:focus {
+      outline: none !important;
+    }
+
+    background: #ff2d55 !important;
+
+    padding: 12px 34px !important;
+    box-sizing: border-box;
+  }
+`;
+
+const EndButton = styled.button`
+  &&& {
+    border-radius: 5px !important;
+    border: 1px solid #bebebe !important;
+    &:focus {
+      outline: none !important;
+    }
+    background: transparent !important;
+
+    padding: 12px 34px !important;
+    box-sizing: border-box;
+  }
+`;
 
 export class ConnectionsError extends React.Component {
   render() {
@@ -21,7 +77,7 @@ export class ConnectionsError extends React.Component {
     return (
       <ErrorWrap>
         <ButtonsWrap>
-          <ErrorImg />
+          <img src={Image} alt="error" />
           <ErrorHeader>Соединение прервано</ErrorHeader>
           <ErrorDescription>
             Проверьте подключение к сети и попробуйте возобновить трансляцию
