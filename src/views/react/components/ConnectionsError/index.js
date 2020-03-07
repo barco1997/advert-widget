e241676a602f2dfdad5a7ecb8480500805c94d97
valdeadmario <vladimir.vstup@gmail.com>
2020-03-07 13:24:40 +0200

    Fix styles

--- src/views/react/components/ConnectionsError/index.js
+++ src/views/react/components/ConnectionsError/index.js
@@ -19,6 +19,7 @@ const ButtonsWrap = styled.div`
   &&& {
     display: flex !important;
     justify-content: space-between !important;
+    width: 288px !important;
 
     font-weight: 500;
     font-size: 12px;
@@ -30,15 +31,25 @@ const ErrorHeader = styled.h3`
   &&& {
     font-weight: 800 !important;
     font-size: 24px !important;
-    line-height: 140% !important;
+    line-height: 28px !important;
+
+    margin-top: 5px !important;
+    margin-bottom: 12px !important;
+    padding: 0 !important;
   }
 `;
 
 const ErrorDescription = styled.p`
   &&& {
+    width: 289px !important;
+
     font-weight: 600 !important;
     font-size: 14px !important;
-    line-height: 170% !important;
+    line-height: 28px !important;
+    text-align: center !important;
+
+    margin: 0 !important;
+    margin-bottom: 22px !important;
   }
 `;
 
@@ -51,37 +62,45 @@ const RefreshButton = styled.button`
     }
 
     background: #ff2d55 !important;
+    color: #ffffff !important;
 
-    padding: 12px 34px !important;
+    padding: 12px 30px !important;
     box-sizing: border-box;
+    box-shadow: none !important;
+
+    margin: 0 !important;
   }
 `;
 
 const EndButton = styled.button`
   &&& {
     border-radius: 5px !important;
-    border: 1px solid #bebebe !important;
+    border: 1px solid #ffffff !important;
     &:focus {
       outline: none !important;
     }
     background: transparent !important;
+    color: #ffffff !important;
 
     padding: 12px 34px !important;
     box-sizing: border-box;
+    box-shadow: none !important;
+
+    margin: 0 !important;
   }
 `;
 
-export class ConnectionsError extends React.Component {
+class ConnectionsError extends React.Component {
   render() {
     const { renewStream, endStream } = this.props;
     return (
       <ErrorWrap>
+        <img src={Image} alt="error" />
+        <ErrorHeader>Соединение прервано</ErrorHeader>
+        <ErrorDescription>
+          Проверьте подключение к сети и попробуйте возобновить трансляцию
+        </ErrorDescription>
         <ButtonsWrap>
-          <img src={Image} alt="error" />
-          <ErrorHeader>Соединение прервано</ErrorHeader>
-          <ErrorDescription>
-            Проверьте подключение к сети и попробуйте возобновить трансляцию
-          </ErrorDescription>
           <RefreshButton onClick={renewStream}>Возобновить</RefreshButton>
           <EndButton onClick={endStream}>Завершить</EndButton>
         </ButtonsWrap>
