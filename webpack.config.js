702d62a2b5547c75daa9ac6b5caf1e73c5ff6511
Danila Kunichnik <danilsk@yandex.ru>
2020-05-06 20:32:14 +0500

    Configured webpack to use chunks and ignore moment locales

--- webpack.config.js
+++ webpack.config.js
@@ -8,62 +8,63 @@ var SRC = path.resolve(__dirname, "./src/main.js");
 //const DIR_NAME = path.join(__dirname, "..");
 
 module.exports = env => {
-  const isDevBuild = !(env && env.prod);
-  const result = dotenv.config();
+    const isDevBuild = !(env && env.prod);
+    const result = dotenv.config();
 
-  if (result.error) {
-    throw result.error;
-  }
-
-  //console.log(result.parsed);
-  return [
-    {
-      entry: SRC,
-
-      output: {
-        filename: "eyezonwidget.js",
-        path: path.resolve(bundleOutputDir)
-      },
-      devServer: {
-        contentBase: bundleOutputDir
-      },
-
-      plugins: isDevBuild
-        ? [
-            new webpack.SourceMapDevToolPlugin(),
-            new copyWebpackPlugin([{ from: "demo/" }])
-          ]
-        : [new webpack.optimize.UglifyJsPlugin()],
-      module: {
-        rules: [
-          { test: /\.html$/i, use: "html-loader" },
-          {
-            test: /\.css$/i,
-            use: [
-              "style-loader",
-              "css-loader" + (isDevBuild ? "" : "?minimize")
-            ]
-          },
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader"
-            }
-          },
-          {
-            test: /\.(jpe?g|png|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-            use: "base64-inline-loader?name=[name].[ext]"
-          },
-          {
-            test: /\.mp3$/,
-            loader: "file-loader",
-            options: {
-              name: "[path][name].[ext]"
-            }
-          }
-        ]
-      }
+    if (result.error) {
+        throw result.error;
     }
-  ];
+
+    //console.log(result.parsed);
+    return [
+        {
+            entry: {
+                index: SRC
+            },
+            devServer: {
+                contentBase: bundleOutputDir
+            },
+            output: {
+                filename: 'eyezonwidget.js',
+                chunkFilename: '[name].bundle.js',
+                path: path.resolve(bundleOutputDir),
+            },
+            plugins: [...isDevBuild ? [
+                    new webpack.SourceMapDevToolPlugin(),
+                    new copyWebpackPlugin([{from: "demo/"}])
+                ] : [],
+                new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+            ],
+            module: {
+                rules: [
+                    {test: /\.html$/i, use: "html-loader"},
+                    {
+                        test: /\.css$/i,
+                        use: [
+                            "style-loader",
+                            "css-loader" + (isDevBuild ? "" : "?minimize")
+                        ]
+                    },
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        use: {
+                            loader: "babel-loader"
+                        }
+                    },
+                    {
+                        test: /\.(jpe?g|png|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                        use: "base64-inline-loader?name=[name].[ext]"
+                    },
+                    {
+                        test: /\.mp3$/,
+                        loader: "file-loader",
+                        options: {
+                            name: "[path][name].[ext]"
+                        }
+                    }
+                ]
+            }
+        }
+    ];
 };
